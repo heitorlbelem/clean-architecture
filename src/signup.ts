acadb068a6d8092ece0734a3b115c68f84dca708
Heitor Belem <heitorlbelem98@gmail.com>
2024-09-10 00:33:01 -0300

    Cria testes para a rota signup

--- src/signup.ts
+++ src/signup.ts
@@ -1,6 +1,6 @@
 import crypto from "crypto";
-import pgp from "pg-promise";
 import express from "express";
+import pgp from "pg-promise";
 import { validateCpf } from "./validateCpf";
 
 const app = express();
@@ -9,62 +9,18 @@ app.use(express.json());
 app.post("/signup", async function (req, res) {
 	const input = req.body;
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
-	try {
-		const id = crypto.randomUUID();
-		let result;
-		const [acc] = await connection.query("select * from ccca.account where email = $1", [input.email]);
-		if (!acc) {
 
-			if (input.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
-				if (input.email.match(/^(.+)@(.+)$/)) {
+	const id = crypto.randomUUID();
+	const [accountData] = await connection.query("select * from ccca.account where email = $1", [input.email]);
+	if (accountData) { return res.status(422).json({ message: -4 }) }
+	if (!input.name.match(/[a-zA-Z] [a-zA-Z]+/)) { return res.status(422).json({ message: -3 }) }
+	if (!input.email.match(/^(.+)@(.+)$/)) { return res.status(422).json({ message: -2 }) }
+	if (!validateCpf(input.cpf)) { return res.status(422).json({ message: -1 }) }
+	if (input.isDriver && !input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) { return res.status(422).json({ message: -5 }) }
 
-					if (validateCpf(input.cpf)) {
-						if (input.isDriver) {
-							if (input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
-								await connection.query("insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver, input.password]);
-								
-								const obj = {
-									accountId: id
-								};
-								result = obj;
-							} else {
-								// invalid car plate
-								result = -5;
-							}
-						} else {
-							await connection.query("insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver, input.password]);
-
-							const obj = {
-								accountId: id
-							};
-							result = obj;
-						}
-					} else {
-						// invalid cpf
-						result = -1;
-					}
-				} else {
-					// invalid email
-					result = -2;
-				}
-
-			} else {
-				// invalid name
-				result = -3;
-			}
-
-		} else {
-			// already exists
-			result = -4;
-		}
-		if (typeof result === "number") {
-			res.status(422).json({ message: result });
-		} else {
-			res.json(result);
-		}
-	} finally {
-		await connection.$pool.end();
-	}
+	await connection.query("insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver, input.password]);
+	res.json({ accountId: id })
+	await connection.$pool.end();
 });
 
 app.listen(3000);
---
+++ test/signup.test.ts
@@ -0,0 +1,75 @@
+import axios from "axios";
+
+axios.defaults.validateStatus = function () {
+  return true;
+}
+
+test("Deve criar uma conta de passageiro", async function () {
+  const input = {
+    name: 'John Doe',
+    email: `john@sample${Math.random()}.com`,
+    cpf: '01234567890',
+    password: '123456',
+    isPassenger: true,
+    isDriver: false
+  }
+  const signupResponse = await axios.post("http://localhost:3000/signup", input);
+  const signupData = signupResponse.data
+  expect(signupData.accountId).toBeDefined()
+});
+
+test("Não deve criar uma conta de passageiro com cpf inválido", async function () {
+  const input = {
+    name: 'John Doe',
+    email: `john@sample${Math.random()}.com`,
+    cpf: '01234567891',
+    password: '123456',
+    isPassenger: true,
+  }
+  const signupResponse = await axios.post("http://localhost:3000/signup", input);
+  const signupData = signupResponse.data
+  expect(signupResponse.status).toBe(422)
+  expect(signupData.message).toBe(-1)
+});
+
+test("Não deve criar uma conta de passageiro com nome inválido", async function () {
+  const input = {
+    name: 'John',
+    email: `john@sample${Math.random()}.com`,
+    cpf: '01234567890',
+    password: '123456',
+    isPassenger: true,
+  }
+  const signupResponse = await axios.post("http://localhost:3000/signup", input);
+  const signupData = signupResponse.data
+  expect(signupResponse.status).toBe(422)
+  expect(signupData.message).toBe(-3)
+});
+
+test("Não deve criar uma conta de passageiro com email inválido", async function () {
+  const input = {
+    name: 'John Doe',
+    email: `john${Math.random()}sample.com`,
+    cpf: '01234567890',
+    password: '123456',
+    isPassenger: true,
+  }
+  const signupResponse = await axios.post("http://localhost:3000/signup", input);
+  const signupData = signupResponse.data
+  expect(signupResponse.status).toBe(422)
+  expect(signupData.message).toBe(-2)
+});
+
+test("Não deve criar uma conta de motorista com placa inválida", async function () {
+  const input = {
+    name: 'John Doe',
+    email: `john@sample${Math.random()}.com`,
+    cpf: '01234567890',
+    password: '123456',
+    isDriver: true,
+    carPlate: 'AAA999'
+  }
+  const signupResponse = await axios.post("http://localhost:3000/signup", input);
+  const signupData = signupResponse.data
+  expect(signupData.message).toBe(-5)
+});
