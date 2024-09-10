eb89f4afea1bd256b70f5ce16b41b75275b38e65
Heitor Belem <heitorlbelem98@gmail.com>
2024-09-10 01:31:38 -0300

    Cria classes para fazer inversão de dependência entre app e recurso
    O app não conhece/depende mais diretamente do AccountDAO, e passa a conhecer apenas o contrato IAccountDAO (interface).

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,17 @@
 import express from "express";
-import { signUp } from "./signup";
+import { AccountDAODatabase } from "./AccountDAO";
+import { Signup } from "./Signup";
 
 const app = express();
 app.use(express.json());
 
+const accountDAO = new AccountDAODatabase()
+const signup = new Signup(accountDAO)
+
 app.post("/signup", async function (req, res) {
   const input = req.body;
   try {
-    const response = await signUp(input)
+    const response = await signup.execute(input)
     res.json(response)
   } catch(e: any) {
     return res.status(422).json({ message: e.message })
--- src/signup.ts
+++ src/signup.ts
@@ -1,15 +1,31 @@
-import { AccountDAO } from "./AccountDAO";
+import { IAccountDAO } from "./AccountDAO";
 import { validateCpf } from "./validateCpf";
 
-export async function signUp (input: any) {
-	const accountDAO = new AccountDAO()
-	const accountData = await accountDAO.getUserByEmail(input.email)
-	if (accountData) { throw new Error("Account already exists") }
-	if (!input.name.match(/[a-zA-Z] [a-zA-Z]+/)) { throw new Error("Invalid name") }
-	if (!input.email.match(/^(.+)@(.+)$/)) { throw new Error("Invalid e-mail") }
-	if (!validateCpf(input.cpf)) { throw new Error("Invalid CPF") }
-	if (input.isDriver && !input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) { throw new Error("Invalid car plate") }
+export class Signup {
+	constructor(private readonly accountDAO: IAccountDAO) {
+		this.accountDAO = accountDAO;
+	}
 
-	const accountId = await accountDAO.saveUser(input)
-	return { accountId }
-};
+	async execute (input: any) {
+		const account = {
+			id: crypto.randomUUID(),
+			name: input.name, 
+			email: input.email,
+			cpf: input.cpf,
+			isDriver: input.isDriver,
+			isPassenger: input.isPassenger,
+			carPlate: input.carPlate,
+			password: input.password
+		}
+		const accountExists = await this.accountDAO.getUserByEmail(account.email)
+		if(accountExists) { throw new Error("Account already exists") }
+		if(!account.name.match(/[a-zA-Z] [a-zA-Z]+/)) { throw new Error("Invalid name") }
+		if(!account.email.match(/^(.+)@(.+)$/)) { throw new Error("Invalid e-mail") }
+		if(!validateCpf(account.cpf)) { throw new Error("Invalid CPF") }
+		if(account.isDriver && !account.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
+			throw new Error("Invalid car plate")
+		}
+		await this.accountDAO.saveUser(account)
+		return { accountId: account.id }
+	}
+}
